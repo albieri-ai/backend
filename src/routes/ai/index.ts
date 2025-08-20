e3366d4bda0f95772b7f2120bfcaf0251143f688
Thiago Ferolla <thiagofunascimento@gmail.com>
2025-08-19 21:32:35 -0300

    Add AI route for generating objective text from user input

---
+++ src/routes/ai/index.ts
@@ -0,0 +1,44 @@
+import { generateText } from "ai";
+import type { FastifyInstance, FastifyServerOptions } from "fastify";
+import { createGroq } from "@ai-sdk/groq";
+import { z } from "zod";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+
+export default function (
+	fastify: FastifyInstance,
+	_opts: FastifyServerOptions,
+) {
+	const groq = createGroq({
+		apiKey: fastify.config.GROQ_API_KEY,
+	});
+	const gemini = createGoogleGenerativeAI({
+		apiKey: fastify.config.GEMINI_API_KEY,
+	});
+
+	fastify.post<{ Body: { text: string } }>(
+		"/objective",
+		{
+			schema: {
+				body: z.object({
+					text: z.string(),
+				}),
+			},
+		},
+		async (request, reply) => {
+			const { text } = await generateText({
+				model: groq("llama-3.1-8b-instant"),
+				system: `\n
+      Com base nas informações que o usuário fornece, você gerará uma frase curta que resumirá o objetivo do usuário.
+      Essa frase será usada como prompt para um modelo de linguagem que responderá seguindo as diretrizes do usuário.
+      Escreva uma única frase condensando tudo que o usuário enviou
+
+      - certifique-se de que não tenha mais de 500 caracteres
+      - não use aspas ou dois pontos
+      - responda apenas com a frase gerada e nada mais`,
+				prompt: request.body.text,
+			});
+
+			reply.send({ data: { objective: text } });
+		},
+	);
+}
