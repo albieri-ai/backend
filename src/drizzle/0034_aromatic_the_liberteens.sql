e78776375008b4648e238862171c321f156d4141
Thiago Ferolla <thiagofunascimento@gmail.com>
2025-09-29 19:30:31 -0300

    updated user_messages view

---
+++ src/drizzle/0034_aromatic_the_liberteens.sql
@@ -0,0 +1,14 @@
+-- Custom SQL migration file, put your code below! --
+CREATE OR REPLACE VIEW user_messages AS (
+SELECT
+        author,
+        persona,
+        (msg->'metadata'->>'createdAt')::timestamp AS date,
+        msg->>'content' AS content,
+        threads.id AS thread,
+        array_length(regexp_split_to_array(msg->>'content', '\s+'), 1)::INTEGER AS word_count
+    FROM threads,
+        LATERAL jsonb_array_elements(messages) AS msg
+    WHERE msg->>'role' = 'user'
+    AND msg->'metadata' ? 'createdAt'
+);
